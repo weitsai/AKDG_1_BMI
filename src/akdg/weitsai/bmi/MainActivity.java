9d3d80b90f30a210b0b7761df81447f7d81eb0bd
weitsai <begining1003@gmail.com>
2014-04-15 11:06:01 +0800

    新增 java 和元件的對應關係

--- src/akdg/weitsai/bmi/MainActivity.java
+++ src/akdg/weitsai/bmi/MainActivity.java
@@ -1,23 +1,30 @@
 package akdg.weitsai.bmi;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+	private EditText editHeight, editWidth;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		findViews();
+	}
 
+	private void findViews() {
+		editHeight = (EditText) findViewById(R.id.edit_height);
+		editWidth = (EditText) findViewById(R.id.edit_width);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
 }
