6546a605cabf2570eea78d8b8fd337fc6f6cb566
weitsai <begining1003@gmail.com>
2014-04-15 12:05:08 +0800

    新增判斷身高或體重是否有為空值的判斷

--- src/akdg/weitsai/bmi/MainActivity.java
+++ src/akdg/weitsai/bmi/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	private EditText editHeight, editWidth;
@@ -13,7 +14,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		findViews();
+		 findViews();
 	}
 
 	private void findViews() {
@@ -27,9 +28,17 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
-	public void submit(View v){
-		
+
+	public void submit(View v) {
+		 String height = editHeight.getText().toString();
+		 String width = editWidth.getText().toString();
+		 if ("".equals(height) | "".equals(width)) {
+		 showEditNullToast();
+		 }
+	}
+
+	private void showEditNullToast() {
+		Toast.makeText(this, "請輸入身高和體重", Toast.LENGTH_SHORT).show();
 	}
 
 }
